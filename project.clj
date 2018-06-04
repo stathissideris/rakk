219f7ac2e3375ef0b78285302c1ac93dd70318f5
Stathis Sideris <stathis@pixelated-noise.com>
2018-06-04 20:23:22 +0300

    Added deps.edn

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [aysylu/loom "1.0.1"]
-                 [kubiq "0.1.0-SNAPSHOT"]])
+                 [aysylu/loom "1.0.1"]])
